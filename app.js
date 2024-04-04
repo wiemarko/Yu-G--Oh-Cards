d8d337b0d45cb71f5717f7546f40f490af3895cf
wiemarko <81916976+wiemarko@users.noreply.github.com>
2024-04-04 23:01:43 +0300

    Flip
    Flip butonunu aktifleştirdim

--- app.js
+++ app.js
@@ -13,3 +13,21 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+
+       // Flip butonu kodları
+document.getElementById('flip').addEventListener('click', function() {
+    const images = document.querySelectorAll('#images');
+    images.forEach(image => {
+        if (image.classList.contains('on-taraf')) {
+            image.style.transform = 'rotateY(180deg)';
+            image.classList.remove('on-taraf');
+            image.classList.add('arka-taraf');
+        } else {
+            image.style.transform = 'rotateY(0deg)';
+            image.classList.remove('arka-taraf');
+            image.classList.add('on-taraf');
+        }
+    });
+});
