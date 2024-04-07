2728e32bc7a0aa9f1d802850878e4de43c034135
wiemarko <81916976+wiemarko@users.noreply.github.com>
2024-04-07 23:21:39 +0300

    day-3

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const resimler = document.querySelectorAll(".on-taraf");
-
-    resimler.forEach(function(card) {
-        resim.addEventListener("click", function() {
-            // Tüm resimlerden aktif sınıfı kaldır
-            resimler.forEach(function(card) {
-                resim.classList.remove("active");
-            });
-
-            // Tıklanan resme aktif sınıfı ekle
-            this.classList.add("active");
-        });
-    });
-});
-
-
-
        // Flip butonu kodları
 document.getElementById('flip').addEventListener('click', function() {
     const images = document.querySelectorAll('#images');
